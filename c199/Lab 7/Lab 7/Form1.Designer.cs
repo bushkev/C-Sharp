0ae3459f7fb373e751b2f9cba8294bde13b6176a
bushkev <32338133+bushkev@users.noreply.github.com>
2019-04-19 12:13:44 -0400

    Add files via upload

---
+++ c199/Lab 7/Lab 7/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Lab_7
+{
+    partial class Lab7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFutureValue = new System.Windows.Forms.Label();
+            this.inputFutVal = new System.Windows.Forms.TextBox();
+            this.labelIntRate = new System.Windows.Forms.Label();
+            this.inputIntRate = new System.Windows.Forms.TextBox();
+            this.labelNumYear = new System.Windows.Forms.Label();
+            this.inputNumYear = new System.Windows.Forms.TextBox();
+            this.labelPresentValue = new System.Windows.Forms.Label();
+            this.outputPresentValue = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFutureValue
+            // 
+            this.labelFutureValue.AutoSize = true;
+            this.labelFutureValue.Location = new System.Drawing.Point(35, 26);
+            this.labelFutureValue.Name = "labelFutureValue";
+            this.labelFutureValue.Size = new System.Drawing.Size(70, 13);
+            this.labelFutureValue.TabIndex = 0;
+            this.labelFutureValue.Text = "Future Value:";
+            // 
+            // inputFutVal
+            // 
+            this.inputFutVal.Location = new System.Drawing.Point(172, 23);
+            this.inputFutVal.Name = "inputFutVal";
+            this.inputFutVal.Size = new System.Drawing.Size(100, 20);
+            this.inputFutVal.TabIndex = 1;
+            // 
+            // labelIntRate
+            // 
+            this.labelIntRate.AutoSize = true;
+            this.labelIntRate.Location = new System.Drawing.Point(35, 52);
+            this.labelIntRate.Name = "labelIntRate";
+            this.labelIntRate.Size = new System.Drawing.Size(107, 13);
+            this.labelIntRate.TabIndex = 2;
+            this.labelIntRate.Text = "Annual Interest Rate:";
+            // 
+            // inputIntRate
+            // 
+            this.inputIntRate.Location = new System.Drawing.Point(172, 49);
+            this.inputIntRate.Name = "inputIntRate";
+            this.inputIntRate.Size = new System.Drawing.Size(100, 20);
+            this.inputIntRate.TabIndex = 3;
+            // 
+            // labelNumYear
+            // 
+            this.labelNumYear.AutoSize = true;
+            this.labelNumYear.Location = new System.Drawing.Point(35, 78);
+            this.labelNumYear.Name = "labelNumYear";
+            this.labelNumYear.Size = new System.Drawing.Size(92, 13);
+            this.labelNumYear.TabIndex = 4;
+            this.labelNumYear.Text = "Number of Years: ";
+            // 
+            // inputNumYear
+            // 
+            this.inputNumYear.Location = new System.Drawing.Point(172, 75);
+            this.inputNumYear.Name = "inputNumYear";
+            this.inputNumYear.Size = new System.Drawing.Size(100, 20);
+            this.inputNumYear.TabIndex = 5;
+            // 
+            // labelPresentValue
+            // 
+            this.labelPresentValue.AutoSize = true;
+            this.labelPresentValue.Location = new System.Drawing.Point(35, 103);
+            this.labelPresentValue.Name = "labelPresentValue";
+            this.labelPresentValue.Size = new System.Drawing.Size(79, 13);
+            this.labelPresentValue.TabIndex = 6;
+            this.labelPresentValue.Text = "Present Value: ";
+            // 
+            // outputPresentValue
+            // 
+            this.outputPresentValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputPresentValue.Location = new System.Drawing.Point(172, 98);
+            this.outputPresentValue.Name = "outputPresentValue";
+            this.outputPresentValue.Size = new System.Drawing.Size(100, 23);
+            this.outputPresentValue.TabIndex = 7;
+            this.outputPresentValue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(98, 135);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // Lab7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 169);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.outputPresentValue);
+            this.Controls.Add(this.labelPresentValue);
+            this.Controls.Add(this.inputNumYear);
+            this.Controls.Add(this.labelNumYear);
+            this.Controls.Add(this.inputIntRate);
+            this.Controls.Add(this.labelIntRate);
+            this.Controls.Add(this.inputFutVal);
+            this.Controls.Add(this.labelFutureValue);
+            this.Name = "Lab7";
+            this.Text = "Future money";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFutureValue;
+        private System.Windows.Forms.TextBox inputFutVal;
+        private System.Windows.Forms.Label labelIntRate;
+        private System.Windows.Forms.TextBox inputIntRate;
+        private System.Windows.Forms.Label labelNumYear;
+        private System.Windows.Forms.TextBox inputNumYear;
+        private System.Windows.Forms.Label labelPresentValue;
+        private System.Windows.Forms.Label outputPresentValue;
+        private System.Windows.Forms.Button calcButton;
+    }
+}
+
---
+++ c199/Lab 7/Lab 7/Form1.cs
@@ -0,0 +1,66 @@
+﻿/* W8172
+ * CIS-199-01
+ * Lab 7
+ * 1 April 2018
+ * Windows Forms application that applies the Net Present Value formula from Finance to calculate how much money you need to invest today to earn a certain amount in the future. 
+ * Your form will gather inputs for the future value (F), the annual interest rate (r) - expressed as a floating point value (like 0.05 for 5%), and the number of years in the future (n) that interest will compound. 
+ * Your application will calculate the present value (P) that you'd need to invest today to earn the desired amount of money in the future.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+
+        /* precondition: positive number value entered into all 3 text boxes, and button clicked. 
+         * postcondition: future value is divided by the 1 plus the interest rate raised to the number of years to get the present value needed.
+         * Present value is then output to the blank label.
+        */
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            double future;                          //variable that will hold the input value for the future value.
+            double rate;                            //variable that will hold the input value for the interest rate.
+            int years;                              //variable that will hold the input value for the number of years.    
+            double present;                         //variable that will hold the output value for the present value.
+
+            //checks to make sure input is valid and store the proper input into the proper variable.
+            if ((double.TryParse(inputFutVal.Text, out future) && future >= 0) 
+                && (double.TryParse(inputIntRate.Text, out rate) && rate > 0) 
+                && (int.TryParse(inputNumYear.Text, out years) && years >= 0))
+            {
+                //Method call that stores the returned value into the proper variable.
+                present = CalcPresentValue(future, rate, years);
+
+                //outputs the present value as currency to the proper label.
+                outputPresentValue.Text = $"{present:c}";
+                
+            }
+            //Displays error message for improper inputs.
+            else
+                MessageBox.Show("Enter valid numbers!!!");
+        }
+
+        /* precondition: 0 <= future amount, 0 < interest rate, and 0 <= number of years. 
+         * postcondition: future value is divided by the 1 plus the interest rate raised to the number of years to get the present value needed.
+         * Present value is then returned.
+        */
+        public static double CalcPresentValue(double future, double rate, int years)
+        {
+            return future / Math.Pow((1 + rate), years);
+        }
+    }
+}
